494cf9521ec779d214294f1faae014a549853239
Mike Boers <github@mikeboers.com>
2013-12-12 15:34:35 -0800

    `ficonfig host-string [--no-password] <base>`

--- ficonfig/commands/host_string.py
+++ ficonfig/commands/host_string.py
@@ -4,7 +4,11 @@ from .main import command, argument
 
 @command(
     argument('base', nargs=1),
+    argument('--no-user', action='store_true'),
+    argument('--no-password', action='store_true'),
+    argument('--no-port', action='store_true'),
     name='host-string',
+    help='construct user@host:port from a base',
 )
 def host_string(args, config):
 
@@ -22,13 +26,19 @@ def host_string(args, config):
         print >> sys.stderr, 'missing', e.args[0]
         exit(1)
 
-    port = config.get(base + 'PORT')
-    if port is not None:
-        host = '%s:%s' % (host, port)
+    if not args.no_port:
+        port = config.get(base + 'PORT')
+        if port is not None:
+            host = '%s:%s' % (host, port)
 
-    user = config.get(base + 'USER')
-    if user is not None:
-        host = '%s@%s' % (user, host)
+    if not args.no_user:
+        user = config.get(base + 'USER')
+        if user is not None:
+            if not args.no_password:
+                password = config.get(base + 'PASSWORD')
+                if password is not None:
+                    user = '%s:%s' % (user, password)
+            host = '%s@%s' % (user, host)
 
     print host
-    
+
--- tests/test_host_string.py
+++ tests/test_host_string.py
@@ -4,5 +4,7 @@ from common import *
 class TestHostString(TestCase):
 
     def test_aaa(self):
-        self.assertEqual('alice@a.com:12345', run('ficonfig host-string AAA').strip())
+        self.assertEqual('alice:apass@a.com:12345', run('ficonfig host-string AAA').strip())
+        self.assertEqual('alice@a.com:12345', run('ficonfig host-string --no-password AAA').strip())
+        self.assertEqual('alice@a.com', run('ficonfig host-string --no-password --no-port AAA').strip())
 
