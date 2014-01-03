286a7bffa94f1e8ac5b61cc10fcaea3365564160
Mike Boers <github@mikeboers.com>
2014-01-03 00:10:55 -0800

    Fix --list --prefix

--- ficonfig/commands/list.py
+++ ficonfig/commands/list.py
@@ -3,7 +3,6 @@ from ..utils import shell_quote
 
 @command(
     argument('-s', '--shell', action='store_true', help='format for a shell'),
-    argument('-p', '--prefix', default='FICONFIG_', help='prefix for shell variables'),
     name='list',
     help='list all key-value pairs',
 )
@@ -11,7 +10,7 @@ def list_(args, config):
 
     for k, v in sorted(config.iteritems()):
         if args.shell:
-            print '%s%s=%s' % (args.prefix, k, shell_quote(v))
+            print '%s%s=%s' % (args.prefix if args.prefix is not None else 'FICONFIG_', k, shell_quote(v))
         else:
             print '%s = %r' % (k, v)
 
